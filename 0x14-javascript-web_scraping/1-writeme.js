cbd134679b8e7de154a1d33ad0d588fa20eee1bc
Gentletouch1 <adeyemishobukola@gmail.com>
2023-10-24 14:42:03 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
-
-// a script that writes a string to a file.
-
-// Import error medule and assign variables
 const fs = require('fs');
-const filePath = process.argv[2];
-const fileContent = process.argv[3];
-
-// prints out the content of the file
-fs.writeFile(filePath, fileContent, 'utf8', function (error) {
-  // Checks for error
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(fileContent);
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
