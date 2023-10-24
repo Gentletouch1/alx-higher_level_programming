afaed0e44e30b40f311b1dcb3cd01cb2fa8653bb
Gentletouch1 <adeyemishobukola@gmail.com>
2023-10-24 14:51:47 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,33 +1,20 @@
 #!/usr/bin/node
-
-// a script that prints all characters of a Star Wars movie:
-
 const request = require('request');
-
-const baseApiUrl = 'https://swapi.dev/api';
-
-const movieId = process.argv[2];
-
-const movieUrl = `${baseApiUrl}/films/${movieId}/`;
-
-request.get(movieUrl, (error, response, body) => {
-  // checks for error
-  if (error) {
-    console.error(`Error: ${error}`);
-    return;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-
-  const characters = JSON.parse(body).characters;
-
-  characters.forEach((characterUrl) => {
-    request.get(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(`Error: ${error}`);
-        return;
-      }
-
-      const characterName = JSON.parse(body).name;
-      console.log(characterName);
-    });
-  });
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
