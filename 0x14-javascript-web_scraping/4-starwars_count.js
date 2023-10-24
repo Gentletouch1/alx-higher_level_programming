7bee40f8f7c57fe6ce2a22a9c579041736b2e57d
Gentletouch1 <adeyemishobukola@gmail.com>
2023-10-24 14:45:05 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,12 @@
 #!/usr/bin/node
-// script that should print the number of movies where the character "Wedge Antilles"
 const request = require('request');
-
-const apiUrl = process.argv[2];
-const characterId = '18';
-
-// Quering the api
-request(apiUrl, function (error, response, body) {
-  if (error) {
-    // check for error
-    console.error(error);
-  } else {
-    const films = JSON.parse(body).results;
-    const numFilmsWithWedge = films.filter((film) => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
-    console.log(numFilmsWithWedge);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
